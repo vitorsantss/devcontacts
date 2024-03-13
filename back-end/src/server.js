0a7fe7094b9b51106858cb8026c62626e472ef87
Vitor Santos <144491159+vitorsantss@users.noreply.github.com>
2024-03-13 15:07:39 -0300

    Update server.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Devcontacts - API\n\nServer is running and listening on\nhttp://localhost:${PORT}`);
